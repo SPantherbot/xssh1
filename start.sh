e543e8c9e31c43d3821e4fbe67d39ad4b14a166a
FooL_DragoN <97965352+SPantherbot@users.noreply.github.com>
2023-11-05 02:24:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 source /app/config.sh
 
-# Start SSH service
 service ssh start
-
-# Start NGINX service
 service nginx start
 
-# Set ngrok token
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
-
-# Start ngrok service and redirect output to a log file
 echo "start ngrok service"
 ngrok tcp 22 --log=stdout > ngrok.log
-# Keep the container running
-tail -f /dev/null
